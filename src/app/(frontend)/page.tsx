5bfc5dd6033f82e2c791d454012b61fe9c45f1ff
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2026-06-16 17:04:58 +0100

    finish v1

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -7,6 +7,7 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Divider from './components/Divider'
 import Testimonials from './components/Testimonials'
+export const dynamic = 'force-dynamic' // 👈 Forces the page to fetch data on demand when visited, rather than during build
 export default function Home() {
   return (
     <>
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URI,
+  connectionString: process.env.PRODUCTION_DB,
 });
 
 // Primary Drizzle ORM client instantiated with your relational schema definitions
